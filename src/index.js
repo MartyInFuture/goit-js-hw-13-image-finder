195e46b1509dd520cb3458aa4edf028b5ca687ea
MartyInFuture <martyinfuture@gmail.com>
2021-07-26 21:59:23 +0300

    add another part of hw

--- src/index.js
+++ src/index.js
@@ -2,35 +2,34 @@ import './scss/main.scss';
 import cardTemplates from './templates/cards.hbs';
 import * as _ from 'lodash';
 
-const TEMPLATE_URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${'what-search'}&page=${'page-number'}&per_page=12&key=19128148-e20e1c798475ebdebc23df32e`;
 const gallery = document.querySelector('.gallery');
 const input = document.querySelector('.search-form input');
 let currentPage = 1;
+let inputPerPage = 6;
 let currentInput;
-
-function renderCards(data) {
-  console.log(data.hits);
-  gallery.insertAdjacentHTML('beforeend', cardTemplates(data.hits));
-}
 
 input.addEventListener(
   'input',
   _.debounce(e => {
+    if (!e.target.value) return false;
+    gallery.innerHTML = '';
+    currentPage = 1;
     currentInput = e.target.value;
-    fetchImages(currentInput, currentPage);
+    fetchImages(currentInput, currentPage, inputPerPage);
   }, 500),
 );
 
-const fetchImages = async (currentInput, currentPage) => {
+const fetchImages = async (currentInput, currentPage, inputPerPage) => {
   const response = await fetch(
-    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${currentInput}&page=${currentPage}&per_page=12&key=19128148-e20e1c798475ebdebc23df32e`,
+    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${currentInput}&page=${currentPage}&per_page=${inputPerPage}&key=19128148-e20e1c798475ebdebc23df32e`,
   )
     .then(res => {
       return res.json();
     })
     .then(data => {
-      console.log(data);
-      renderCards(data);
+      if (!data) return false;
+      gallery.insertAdjacentHTML('beforeend', cardTemplates(data.hits));
+      observer.observe(gallery.lastElementChild);
     })
     .catch(error => {
       console.log(error);
@@ -42,14 +41,18 @@ const observer = new IntersectionObserver(
     entries.forEach(item => {
       if (!item.isIntersecting) return false;
       observer.unobserve(item.target);
-      fetchImages(currentInput, (currentPage += 1));
-      observer.observe(gallery.lastElementChild);
+      console.log(gallery.children);
+      // gallery.children[(currentPage - 1) * 12].scrollIntoView({
+      //   behavior: 'smooth',
+      //   block: 'start',
+      // });
+      currentPage += 1;
+      setTimeout(() => {
+        fetchImages(currentInput, currentPage, inputPerPage);
+      }, 500);
     });
   },
   {
-    root: gallery,
-    threshold: 0.5,
+    threshold: 1,
   },
 );
-
-observer.observe(gallery.lastElementChild);
